e832008abcf1df76beb717d4ef794d4f42e13af8
beconomist <beconomist@gmail.com>
2016-05-03 01:03:24 +0800

    First Commit

--- PingConverter/ViewController.swift
+++ PingConverter/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var squareMeterTextField: UITextField!
+    @IBOutlet weak var pingTextField: UITextField!
+    @IBOutlet weak var tatamiTextField: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.squareMeterTextField.delegate = self
+        self.pingTextField.delegate = self
+        self.tatamiTextField.delegate = self
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.sayGoodbye(_:)), name: UIApplicationWillResignActiveNotification, object: nil)
+        
+        let blueView = UIView()
+        self.view.addSubview(blueView)
+        
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calculate(sender: UIButton) {
+    
+        
+        if let sm = Double(squareMeterTextField.text!) {
+            pingTextField.text = String(0.3025 * sm)
+            tatamiTextField.text = String(0.6 * sm)
+        }
+    
+        if let p = Double(pingTextField.text!) {
+            squareMeterTextField.text = String(3.31 * p)
+            tatamiTextField.text = String(2 * p)
+        }
+        
+        if let t = Double(tatamiTextField.text!) {
+            squareMeterTextField.text = String(1.65 * t)
+            pingTextField.text = String(0.5 * t)
+        }
     }
-
-
+        
+    @IBAction func clearValue(sender: UIButton) {
+            squareMeterTextField.text = ""
+            pingTextField.text = ""
+            tatamiTextField.text = ""
+        
+        
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func sayGoodbye(noti: NSNotification) {
+        print("sayGoodBye from \(noti)")
+    }
+    
 }
 
